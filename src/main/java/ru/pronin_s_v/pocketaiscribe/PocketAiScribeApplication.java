6f1b913d3c0a89a8a1724a21bfa4acc6329175aa
SergPronin <sergeypronin.dev@gmail.com>
2026-04-07 13:59:33 +0300

    chore: initial commit

---
+++ src/main/java/ru/pronin_s_v/pocketaiscribe/PocketAiScribeApplication.java
@@ -0,0 +1,13 @@
+package ru.pronin_s_v.pocketaiscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketAiScribeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PocketAiScribeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/pronin_s_v/pocketaiscribe/PocketAiScribeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pronin_s_v.pocketaiscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketAiScribeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
